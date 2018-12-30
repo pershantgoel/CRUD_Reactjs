fd5f2c8addf1759dabb56068b6371395f1487b39
pershantgoel <pershantgoel@gmail.com>
2018-12-30 23:38:08 +0530

    another commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      title : "CRUD Application",
+      act:0,
+      index : "",
+      datas : []
+    }
+    this.fSubmit = this.fSubmit.bind(this);
+    this.fRemove = this.fRemove.bind(this);
+    this.fEdit = this.fEdit.bind(this);
+
+
+  }
+  
+  fSubmit=(e)=>{
+    e.preventDefault();
+    console.log("fn call")
+    let datas = this.state.datas;
+    let name = this.refs.name.value;
+    let address = this.refs.address.value;
+
+    let data = {
+      name , address
+    }
+    datas.push(data);
+    this.setState({datas:datas});
+
+    this.refs.myForm.reset();
+    console.log(this.state.datas);
+
+  }
+  fRemove=(i)=>{
+    let datas = this.state.datas;
+    datas.splice(i,1)
+  }
+  fEdit=(i)=>{
+
+  }
+
   render() {
+    let datas = this.state.datas;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h2>{this.state.title}</h2>
+        <form ref="myForm" className="myForm">
+          <input type="text" ref="name" placeholder="your name" className="formField" autoFocus/>
+          <input type="text" ref="address" placeholder="your address" className="formField"/>
+          <button onClick={(e)=>this.fSubmit(e)} className="myButton">submit</button>
+
+        </form>
+
+      <ul>
+        {console.log("datas",datas)}
+        {datas.map((data,i)=>
+        <li key={i} className="myList">
+          {i+1}.{data.name} , {data.address}
+          <button onClick={()=>this.fRemove(i)} className="myButton">remove</button>
+          <button onClick={()=>this.fEdit(i)} className="myButton">edit</button>        
+
+        </li>
+        )}
+      </ul>
+
       </div>
     );
   }
