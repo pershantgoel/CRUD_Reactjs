0ede70dd0689bb2368ed08dc359252f0d2a01e27
pershantgoel <pershantgoel@gmail.com>
2018-12-31 19:30:30 +0530

    Updates

---
+++ precache-manifest.ddf894d7a4120f0314d9390fdcd1468d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c4788cb59d9941079c0",
+    "url": "/CRUD_Reactjs/static/js/runtime~main.6c4788cb.js"
+  },
+  {
+    "revision": "2df5eca6b93e77709073",
+    "url": "/CRUD_Reactjs/static/js/main.2df5eca6.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/CRUD_Reactjs/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "2df5eca6b93e77709073",
+    "url": "/CRUD_Reactjs/static/css/main.86b4f7d6.chunk.css"
+  },
+  {
+    "revision": "376d2f5263e8d01434572fa8a877eb18",
+    "url": "/CRUD_Reactjs/index.html"
+  }
+];
